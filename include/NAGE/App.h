621c64444f3eac0f11a2d451f3642370b6af0cfc
pgvalle <pgvalle.nogueira@gmail.com>
2024-05-10 18:42:30 -0300

    Changing declaration and implementation files of rendercalls

--- include/NAGE/App.h
+++ include/NAGE/App.h
@@ -2,8 +2,6 @@
 
 #include "Scene.h"
 
-#include <SDL.h>
-
 // no need to redeclare variables in App.cpp file again
 #ifndef EXTERN
 #define EXTERN extern
@@ -32,5 +30,23 @@ void run(Scene *scene);
 
 // so that scenes can close app
 EXTERN bool shouldClose;
+
+/**
+ * Rendering capabilities. Only apply to things offered by this library.
+ * If you use renderer directly, this will not affect it.
+ */
+
+// 0xAARRGGBB
+void setColor(Uint32 color);
+void setColor(Uint8 r, Uint8 g, Uint8 b, Uint8 a = 255);
+
+// See SDL_RendererFlip enum to know more
+void setFlip(SDL_RendererFlip flip);
+
+// to use palettes
+void setBlend(bool blend);
+
+void renderTile(int x, int y, int AtlasX, int atlasY);
+void renderText(int x, int y, const char *text, ...);
 
 #undef EXTERN
