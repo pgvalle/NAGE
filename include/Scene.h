0bc14bc1a4c53096f2481a4945be2b053f44b5b5
pgvalle <pgvalle.nogueira@gmail.com>
2024-05-18 17:07:58 -0300

    docs: removing scene parent explanation in docs

--- include/Scene.h
+++ include/Scene.h
@@ -3,21 +3,6 @@
 
 #include <SDL.h>
 
-/**
- * All scenes, but the main one, have a parent.
- *
- * You implement your own main scene:
- *   class MainScene : public Scene
- *
- * The parenting thing is to allow data access without passing as argument:
- *   childscene::childscene(Scene *urmum) {
- *     this->parent = urmum;
- *   }
- *
- *   void childscene::update(float delta) {
- *     this->parent->field.doSmth();
- *   }
- */
 class Scene
 {
 public:
